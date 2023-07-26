874c77f486d58ffb52e40ca1bfa34360365b20ba
AsmaAljabri <aasmaa.aljabrii@gmail.com>
2023-07-26 17:04:20 +0400

    solve problem 1 and 5

---
+++ problem 5.py
@@ -0,0 +1,11 @@
+ # The inputs are in single line containing the two integers a and b.
+ # using import math to calculate the gcd of the of both inputs
+ 
+import math
+a= int(input(" enter the first integer = "))
+ 
+b= int(input(" enter the second integer = "))
+ 
+print(" the gcd of ",a,b)
+print(math.gcd(a,b))
+ 
