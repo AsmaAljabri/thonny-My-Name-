6ee83514f5da7e95b2c4ff65f3590dfda82e2f6b
AsmaAljabri <aasmaa.aljabrii@gmail.com>
2023-07-26 17:06:01 +0400

    1

---
+++ problem 1.py
@@ -0,0 +1,11 @@
+# The inputs are in single line containing the two integers a and b.
+# ysing the inputs s and t the user enter to calculate the Distance = speed ∗ time
+
+
+s= int(input(" enter the first integer = "))
+ 
+t= int(input(" enter the second integer = "))
+
+d = s * t
+
+print(" the distance is equal to speed times the time Distance = speed",s, "∗",t , "time = " ,d)
