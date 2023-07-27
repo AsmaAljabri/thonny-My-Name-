0b6b27e9fc1003439e0f2a158666be6aa9715e68
AsmaAljabri <aasmaa.aljabrii@gmail.com>
2023-07-27 10:18:38 +0400

    loop

---
+++ # loop (while to repeat a word).py
@@ -0,0 +1,20 @@
+# loop
+# to repeat the word Hello
+
+count = 0
+while( count < 5 ):
+    print("hello")
+    count+=1
+   
+
+# to sum the numbers from 1-10
+
+counter = 1 
+sum = 0
+while (counter <= 10):
+    sum = sum + counter
+    counter += 1
+
+    print(sum )
+
+
